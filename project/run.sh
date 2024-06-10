c5e62cb2fc7129d3a3609630a77921eca9563df9
yimingshi <yimingshi666@gmail.com>
2024-06-10 10:04:38 +0800

    update: run.sh & add tags

--- project/run.sh
+++ project/run.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-# pip install PyYAML ansible 
-ansible-playbook -i inventory setup-and-run-docker.yml
+# pip install -U PyYAML ansible 
+case $1 in
+  install)
+    ansible-playbook -i inventory setup-and-run-docker.yml --tags "install,docker_setup"
+    ;;
+  copy)
+    ansible-playbook -i inventory setup-and-run-docker.yml --tags "copy_image"
+    ;;
+  load)
+    ansible-playbook -i inventory setup-and-run-docker.yml --tags "load_image,tag_image"
+    ;;
+  run)
+    ansible-playbook -i inventory setup-and-run-docker.yml --tags "run_container"
+    ;;
+  all)
+    ansible-playbook -i inventory setup-and-run-docker.yml
+    ;;
+  *)
+    echo "Usage: $0 {install|copy|load|run|all}"
+    exit 1
+    ;;
+esac
